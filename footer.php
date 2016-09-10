9c2a92968fb518f981ed7482e895597b2b1a0ec2
Optix <lavega45@yahoo.fr>
2016-09-10 19:21:30 +0200

    OK

---
+++ footer.php
@@ -0,0 +1,6 @@
+
+<div style="background-color: black; width: auto; height: 100px; bottom: 0; margin-right: auto; margin-left: auto;">
+<p>godsgoidogjpsdkgp,m</p>
+<br />
+<p>vdovisjdvp,sdvldfm,b</p>
+</div>
---
+++ header.php
@@ -0,0 +1,4 @@
+
+<div style="width: auto; margin: auto; height: 100px; background-color: black;">
+
+</div>
