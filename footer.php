500965cfecadb555895e01863357284c033f8b2d
Optix <lavega45@yahoo.fr>
2016-09-10 19:23:20 +0200

    revu des fichiers

--- footer.php
+++ footer.php
@@ -1,6 +1,3 @@
 
 <div style="background-color: black; width: auto; height: 100px; bottom: 0; margin-right: auto; margin-left: auto;">
-<p>godsgoidogjpsdkgp,m</p>
-<br />
-<p>vdovisjdvp,sdvldfm,b</p>
 </div>
